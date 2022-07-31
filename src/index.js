2c127646c9f8d4b7a09b2cca7111ff1e0630c3bd
Anatoli_Z <anatoli.zdanevitch@gmail.com>
2022-07-31 19:14:29 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -38,9 +38,49 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let kek = [];
+    for (i = 0; i< expr.length/10; i++){
+
+        kek.push(expr.slice(i*10,10*(i+1)))
+    }
+    let newMass = [];
+    kek.forEach((val) => {
+        let str ="";
+        for (i =0; i<val.length; i = i+2) {
+            if (val[i] == 1 && val[i+1] == 0) {
+                str += '.'
+            } else {
+                if (val[i] == 1 && val[i+1] == 1){
+                    str += '-'
+                } else {
+                    if (val[i] == "*"){
+                        str += '*'
+                        
+                    }
+                }
+            }
+            
+        }
+        newMass.push(str)
+    });
+    
+
+    let itogo = newMass.map(elem => MORSE_TABLE[elem])
+//arr.map(element => MORSE_TABLE[element])
+// newMass.map(elem = MORSE_TABLE[elem])
+for (i = 0; i< itogo.length; i++) {
+if (itogo[i] == undefined) {
+    itogo[i] = " "
+}
+}
+return itogo.join("")
 }
 
 module.exports = {
     decode
 }
+
+
+decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010")
+
+
